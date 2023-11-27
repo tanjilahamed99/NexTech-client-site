4b90e1910bb0959e2c076d85357582eeae58baf0
Tanjil Ahamed <tanjil113355@gmail.com>
2023-11-27 18:11:06 +0600

    featured review system added

--- src/Pages/DisplayFeaturedDetail/DispalyFeaturedDetail.jsx
+++ src/Pages/DisplayFeaturedDetail/DispalyFeaturedDetail.jsx
@@ -2,10 +2,15 @@ import { useParams } from "react-router-dom";
 import Swal from "sweetalert2";
 import UseAxiosPublic from "../../Hooks/AxiosPublic/UseAxiosPublic";
 import { useQuery } from "@tanstack/react-query";
+import UseAuth from "../../Hooks/useAuth/UseAuth";
+import { useForm } from "react-hook-form";
+import SectionTittle from "../../Shared/SectionTittle/SectionTittle";
 
 const DisplayFeaturedDetail = () => {
     const axiosPublic = UseAxiosPublic()
+    const { register, handleSubmit } = useForm()
     const { id } = useParams()
+    const { user } = UseAuth()
     const { data: featured, refetch } = useQuery({
         queryKey: ['featured', id],
         queryFn: async () => {
@@ -14,7 +19,6 @@ const DisplayFeaturedDetail = () => {
         }
     })
 
-    // console.log(_id)
 
     const handleReport = (id) => {
         Swal.fire({
@@ -42,28 +46,91 @@ const DisplayFeaturedDetail = () => {
         });
     }
 
-    // const 
-    return (
-        <div className="card flex flex-row gap-10 justify-center w-[70%] p-10 my-20 mx-auto shadow-xl">
-            <img className='h-[300px] w-[300px] mx-auto' src={featured?.image} alt="" />
-            <div className="card-body mt-5">
-                <h2 className="text-2xl font-bold ">{featured?.name}</h2>
-                <div className='flex items-center font-semibold text-sm'>
-                    <p>{featured?.brand}</p>
-                    <p>{featured?.upload_date}</p>
-                </div>
-                <h2 className="text-lg">Votes: <span className="font-bold">{featured?.upvote}</span></h2>
-                <hr className='text-black text-lg my-4 border border-black' />
-                <div className='flex items-center w-fit gap-4'>
-                    <p>#{featured?.tags[0]}</p>
-                    <p>#{featured?.tags[1]}</p>
-                    <p>#{featured?.tags[2]}</p>
-                </div>
-                <div className="my-3">
-                    {
-                        featured?.reported ? <button className="btn btn-outline text-red-500">Already Reported</button> : <button onClick={() => handleReport(featured?._id)} className="btn btn-outline text-red-500">Report</button>
-                    }
+    // review
+    const onSubmit = (data) => {
 
+
+        const reviewData = {
+            featuredId: id,
+            featuredName: featured.name,
+            userName: user.displayName,
+            userPhoto: user.photoURL,
+            rating: data.rating
+        }
+
+        axiosPublic.post('/featuredReview', reviewData)
+            .then(res => {
+                if (res.data) {
+                    Swal.fire({
+                        title: "review set successful!",
+                        text: "Your file has been deleted.",
+                        icon: "success"
+                    });
+                }
+            })
+
+    }
+
+    return (
+        <div>
+            <div className="card flex flex-row gap-10 justify-center w-[70%] p-10 my-20 mx-auto shadow-xl">
+                <img className='h-[300px] w-[300px] mx-auto' src={featured?.image} alt="" />
+                <div className="card-body mt-5">
+                    <h2 className="text-2xl font-bold ">{featured?.name}</h2>
+                    <div className='flex items-center font-semibold text-sm'>
+                        <p>{featured?.brand}</p>
+                        <p>{featured?.upload_date}</p>
+                    </div>
+                    <h2 className="text-lg">Votes: <span className="font-bold">{featured?.upvote}</span></h2>
+                    <hr className='text-black text-lg my-4 border border-black' />
+                    <div className='flex items-center w-fit gap-4'>
+                        <p>#{featured?.tags[0]}</p>
+                        <p>#{featured?.tags[1]}</p>
+                        <p>#{featured?.tags[2]}</p>
+                    </div>
+                    <div className="my-3">
+                        {
+                            featured?.reported ? <button className="btn btn-outline text-red-500">Already Reported</button> : <button onClick={() => handleReport(featured?._id)} className="btn btn-outline text-red-500">Report</button>
+                        }
+
+                    </div>
+                </div>
+            </div>
+            <div>
+                {/* review section */}
+                {/* display reviews */}
+                <div>
+
+                </div>
+                {/* review form */}
+                <div className="my-10">
+                    <SectionTittle tittle={'Post Review'} subTittle={'Review'}></SectionTittle>
+                    <form onSubmit={handleSubmit(onSubmit)} className="grid my-10 grid-cols-2 items-center justify-center mx-auto gap-10">
+                        <div className="w-[80%] mx-auto">
+                            <h2 className="font-bold text-sm mb-2">Name</h2>
+                            <input {...register("name")} type="text" placeholder="Type here" className="input input-bordered w-full" value={user.displayName} disabled />
+                        </div>
+                        <div className="w-[80%] mx-auto">
+                            <h2 className="font-bold text-sm mb-2">Photo</h2>
+                            <input {...register("photo")} type="text" placeholder="Type here" className="input input-bordered w-full" value={user.photoURL} disabled />
+                        </div>
+                        <div className="w-[80%] mx-auto">
+                            <h2 className="font-bold text-sm mb-2">Description</h2>
+                            <textarea {...register("desc")} className="textarea textarea-bordered w-full" placeholder="Bio" required></textarea>
+                        </div>
+                        <div className="w-[80%] mx-auto">
+                            <h2 className="font-bold text-sm mb-2">Rating</h2>
+                            <select {...register("rating")} className="select select-bordered w-full" required>
+                                <option disabled>Who shot first?</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                        <input type="submit" value={'Submit'} placeholder="Type here" className="input col-span-2 btn-outline input-bordered w-[90%] mx-auto" />
+                    </form>
                 </div>
             </div>
         </div>
