457c3e0b1da25ed7cc44958ea0a2424e827b93a9
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-12 22:52:14 +0600

    bookmark functionality and routes and pages added

--- src/Hooks/DisplayBookmark/DisplayBookmark.jsx
+++ src/Hooks/DisplayBookmark/DisplayBookmark.jsx
@@ -1,20 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
 import UseAxiosPublic from "../AxiosPublic/UseAxiosPublic";
+import UseAuth from "../useAuth/UseAuth";
 
 const DisplayBookmark = () => {
 
     const axiosPublic = UseAxiosPublic()
 
+    const { user } = UseAuth()
 
-    const { data: bookmark } = useQuery({
+
+    const { data: bookmark, refetch } = useQuery({
         queryKey: ['bookmark'],
         queryFn: async () => {
-            const res = await axiosPublic.get('/bookmark')
+            const res = await axiosPublic.get(`/bookmark/${user?.email}`)
             return res.data
         }
     })
 
-    return [bookmark]
+    return [bookmark, refetch]
 };
 
 export default DisplayBookmark;
--- src/Pages/Dashboard/Bookmark/Bookmark.jsx
+++ src/Pages/Dashboard/Bookmark/Bookmark.jsx
@@ -1,10 +1,46 @@
+import Swal from "sweetalert2";
 import DisplayBookmark from "../../../Hooks/DisplayBookmark/DisplayBookmark";
 import SectionTittle from "../../../Shared/SectionTittle/SectionTittle";
+import UseAxiosPublic from "../../../Hooks/AxiosPublic/UseAxiosPublic";
 
 const Bookmark = () => {
 
-    const [bookmark] = DisplayBookmark()
+    const axiosPublic = UseAxiosPublic()
+
+    const [bookmark,refetch] = DisplayBookmark()
     console.log(bookmark)
+
+    const handleDelete = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                axiosPublic.delete(`/bookmark/${id}`)
+                    .then(res => {
+                        if (res.data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                            refetch()
+                        }
+
+                    })
+
+
+
+            }
+        })
+    }
+
 
     return (
         <div className="mx-auto">
@@ -20,7 +56,7 @@ const Bookmark = () => {
                                 <p>{item?.upload_date}</p>
                             </div>
                             <hr className='text-black text-lg my-4 border border-black' />
-                            <button className="btn btn-outline">Remove from Bookmark</button>
+                            <button onClick={() => handleDelete(item._id)} className="btn btn-outline">Remove from Bookmark</button>
                         </div>
                     </div>)
                 }
