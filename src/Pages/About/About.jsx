2b471318f5e2eff133b78f94a9a22b9c90be6e30
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-25 19:53:29 +0600

    style : team section added

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,9 +1,11 @@
 import AboutBAnner from "./AboutBAnner";
+import OurTeam from "./OurTeam";
 
 const About = () => {
     return (
         <div className="py-20">
             <AboutBAnner></AboutBAnner>
+            <OurTeam></OurTeam>
         </div>
     );
 };
---
+++ src/Pages/About/OurTeam.jsx
@@ -0,0 +1,89 @@
+import SectionTittle from "../../Shared/SectionTittle/SectionTittle";
+
+const OurTeam = () => {
+    return (
+        <div className=" ">
+
+            <div className="bg-center bg-cover hero bg-fixed" style={{ backgroundImage: 'url(https://i.ibb.co/ymCQwfg/mujan-news.jpg)' }}>
+                <div className="hero-overlay bg-opacity-50"></div>
+                <div>
+                    <div className="text-white pt-10">
+                        <SectionTittle subTittle={'Team'} tittle={'Our Team Members'}></SectionTittle>
+                    </div>
+                    <div className="my-4 grid grid-cols-1 pb-20 pt-16 md:grid-rows-2  lg:grid-cols-3 items-center gap-10 bg-center bg-cover  bg-fixed">
+                        <div className="bg-gradient-to-r from-purple-200 to-blue-200 p-5 mx-auto  rounded-2xl">
+                            <div className="relative">
+                                <img className="w-[250px] h-[250px] mx-auto" src="https://i.ibb.co/5kzyH8G/pngwing-com-6.png" alt="" />
+                                <p className="text-sm font-bold absolute top-1 -right-5 bg-purple-600 p-2 rounded-xl text-white">Manager</p>
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <h2 className="text-2xl font-medium">Captain Jack Serow</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-gradient-to-r from-purple-200 to-blue-200 p-5 mx-auto  rounded-2xl">
+                            <div className="relative">
+                                <img className="w-[250px] h-[250px] mx-auto" src="https://i.ibb.co/M8nNyDV/pngwing-com-8.png" alt="" />
+                                <p className="text-sm font-bold absolute top-1 -right-5 bg-purple-600 p-2 rounded-xl text-white">Manager</p>
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <h2 className="text-2xl font-medium">Captain Jack Serow</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-gradient-to-r from-purple-200 to-blue-200 p-5 mx-auto  rounded-2xl">
+                            <div className="relative">
+                                <img className="w-[250px] h-[250px] mx-auto" src="https://i.ibb.co/4JWn5WV/pngwing-com-7.png" alt="" />
+                                <p className="text-sm font-bold absolute top-1 -right-5 bg-purple-600 p-2 rounded-xl text-white">Manager</p>
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <h2 className="text-2xl font-medium">Captain Jack Serow</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-gradient-to-r from-purple-200 to-blue-200 p-5 mx-auto  rounded-2xl">
+                            <div className="relative">
+                                <img className="w-[250px] h-[250px] mx-auto" src="https://i.ibb.co/wcSVKYj/pngwing-com-10.png" alt="" />
+                                <p className="text-sm font-bold absolute top-1 -right-5 bg-purple-600 p-2 rounded-xl text-white">Manager</p>
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <h2 className="text-2xl font-medium">Captain Jack Serow</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-gradient-to-r from-purple-200 to-blue-200 p-5 mx-auto  rounded-2xl">
+                            <div className="relative">
+                                <img className="w-[250px] h-[250px] mx-auto" src="https://i.ibb.co/wcSVKYj/pngwing-com-10.png" alt="" />
+                                <p className="text-sm font-bold absolute top-1 -right-5 bg-purple-600 p-2 rounded-xl text-white">Manager</p>
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <h2 className="text-2xl font-medium">Captain Jack Serow</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-gradient-to-r from-purple-200 to-blue-200 p-5 mx-auto  rounded-2xl">
+                            <div className="relative">
+                                <img className="w-[250px] h-[250px] mx-auto" src="https://i.ibb.co/wcSVKYj/pngwing-com-10.png" alt="" />
+                                <p className="text-sm font-bold absolute top-1 -right-5 bg-purple-600 p-2 rounded-xl text-white">Manager</p>
+                            </div>
+                            <div>
+                                <div className="text-center">
+                                    <h2 className="text-2xl font-medium">Captain Jack Serow</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+// https://i.ibb.co/4JWn5WV/pngwing-com-7.png
+
+export default OurTeam;
