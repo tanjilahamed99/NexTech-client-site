99e7718fdbcb8d79bce4ad4b579bb067ee733226
Tanjil Ahamed <tanjil113355@gmail.com>
2023-12-27 22:31:54 +0600

    style : Keen slider added

--- src/Layout/Home/Home.jsx
+++ src/Layout/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../../Pages/Home/Banner/Banner"
 import CouponBanner from "../../Pages/Home/CouponBanner/CouponBanner";
 import Featured from "../../Pages/Home/Featured/Featured";
 import HomeAbout from "../../Pages/Home/HomeAbout/HomeAbout";
+import Testimonial from "../../Pages/Home/Testimonial/Testimonial";
 import Trending from "../../Pages/Home/Trending/Trending";
 
 
@@ -10,6 +11,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Featured></Featured>
+            <Testimonial></Testimonial>
             <Trending></Trending>
             <CouponBanner></CouponBanner>
             <HomeAbout></HomeAbout>
