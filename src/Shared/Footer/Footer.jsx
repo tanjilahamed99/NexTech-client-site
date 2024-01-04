71a4065f4339a86b72f69a220655b8667be86f0d
Tanjil Ahamed <tanjil113355@gmail.com>
2024-01-04 20:39:05 +0600

    footer new style added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="bg-black ">
             <div className=" text-white items-center p-4 ">
-                <div className="flex flex-col lg:flex-row items-center  md:justify-between">
+                <div className="flex flex-col lg:flex-row md:items-center md:justify-between justify-start gap-3">
                     <div className="flex items-center gap-2">
                         <img className="rounded-lg" src="https://i.ibb.co/g3DmKt8/logo.png" alt="" />
                         <div className="border-r-2 pr-2">
@@ -13,12 +13,12 @@ const Footer = () => {
                             <p className="font-semibold text-sm">Best Website</p>
                         </div>
                     </div>
-                    <div className="flex flex-col md:flex-row md:items-center gap-3">
-                        <h2 className="font-semibold text-lg btn border-none border-r-4 btn-outline text-white">Features</h2>
-                        <h2 className="font-semibold text-lg btn border-none border-r-4 btn-outline text-white">Term & Condition</h2>
-                        <h2 className="font-semibold text-lg btn border-none border-r-4 btn-outline text-white">Security</h2>
-                        <h2 className="font-semibold text-lg btn border-none border-r-4 btn-outline text-white">Plan</h2>
-                        <h2 className="font-semibold text-lg btn border-none border-r-4 btn-outline text-white">Design</h2>
+                    <div className="flex flex-col md:flex-row md:items-center md:gap-10 gap-2 ">
+                        <h2 className="link link-hover">Features</h2>
+                        <h2 className="link link-hover">Term & Condition</h2>
+                        <h2 className="link link-hover">Security</h2>
+                        <h2 className="link link-hover">Plan</h2>
+                        <h2 className="link link-hover">Design</h2>
                     </div>
                 </div>
             </div>
