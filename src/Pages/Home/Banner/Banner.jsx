1ae8ae357692ab32abda215dde457d290efa5410
Tanjil Ahamed <tanjil113355@gmail.com>
2024-03-09 21:36:23 +0600

    i jast added a new slider

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
             <div className="hero-overlay bg-opacity-70 "></div>
             <div className="flex flex-col-reverse lg:py-10 py-5 md:flex-row items-center lg:mt-10 justify-center text-white">
                 <div className="lg:w-1/2 pl-5 w-full">
-                    {/* banne */}
+                    {/* banner */}
                     <div className="w-full">
                         <h1 className="mb-5 text-2xl lg:text-5xl font-bold">NexTech: Unleashing Tomorrows Innovations Today!</h1>
                         <p className="mb-5">Dive into the dynamic world of technology at TechHub, where cutting-edge meets user-friendly. Explore the latest gadgets, stay ahead with insightful reviews, and immerse yourself in the future of tech trends. Your gateway to innovation starts here. Join the digital revolution with TechHub – Where Tomorrow Happens Today!</p>
